6d9a6af62a469af568c91b4619edc1213d796c17
Sebastian <sebastian.galbinita97@gmail.com>
2021-12-04 10:06:45 +0200

    files

---
+++ Settings/settings.h
@@ -0,0 +1,6 @@
+#include<iostream>
+#include<string>
+
+//perform a pwd to where you want to put your files
+std::string PATH="/Users/sebastian/Desktop/C++Libraries/My c++ Project/Texts/";
+std::string EXTENSION=".txt";
